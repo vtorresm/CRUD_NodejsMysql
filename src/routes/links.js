fe97972260c8cec9315246188f3beba0e3b6385d
Victor Torres <vatorresm88@gmail.com>
2019-02-27 19:45:42 -0500

    Views (Template Engine)

--- src/routes/links.js
+++ src/routes/links.js
@@ -3,4 +3,12 @@ const router= express.Router();
 
 const pool = require('../database');
 
+router.get('/add', (req, res) => {
+    res.render('links/add');
+});
+
+router.post('/add', (req, res) => {
+    res.send('received');
+});
+
 module.exports = router;
