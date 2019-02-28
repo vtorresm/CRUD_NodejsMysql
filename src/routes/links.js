da5cd4eb50f35c204b23f393ac0da36c4d815052
Victor Torres <vatorresm88@gmail.com>
2019-02-27 21:15:05 -0500

    Eliminando Datos de Mysql

--- src/routes/links.js
+++ src/routes/links.js
@@ -25,4 +25,12 @@ router.get('/', async (req, res) => {
     res.render('links/list', { links });
 });
 
+router.get('/delete/:id', async (req, res) => {
+    //console.log(req.params.id);
+    //res.send('DELETED');
+    const { id } = req.params;
+    await pool.query('DELETE FROM links WHERE ID = ?', [id]);
+    res.redirect('/links');
+});
+
 module.exports = router;
