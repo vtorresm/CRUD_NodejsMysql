bcf7d147d39f20b49e30950948777449ead9a735
Victor Torres <vatorresm88@gmail.com>
2019-02-27 21:54:31 -0500

    Actualizando Datos de Mysql

--- src/routes/links.js
+++ src/routes/links.js
@@ -33,4 +33,26 @@ router.get('/delete/:id', async (req, res) => {
     res.redirect('/links');
 });
 
+router.get('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    //console.log(id);
+    //res.send('received');
+    const links = await pool.query('SELECT * FROM links WHERE id = ?', [id]);
+    console.log(links[0]);
+    res.render('links/edit', {link: links[0]});
+});
+
+router.post('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const { title, description, url } = req.body;
+    const newLink = {
+        title,
+        description,
+        url
+    };
+    console.log(newLink);
+    await pool.query('UPDATE links set ? WHERE id = ?', [newLink, id]);
+    res.redirect('/links');
+});
+
 module.exports = router;
