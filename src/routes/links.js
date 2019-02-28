49f2cdfb9537b50ec4fe6db33fa80fedb23f9aa5
Victor Torres <vatorresm88@gmail.com>
2019-02-27 20:03:05 -0500

    Guardar datos en Mysql

--- src/routes/links.js
+++ src/routes/links.js
@@ -7,7 +7,15 @@ router.get('/add', (req, res) => {
     res.render('links/add');
 });
 
-router.post('/add', (req, res) => {
+router.post('/add', async (req, res) => {
+    const { title, url, description } = req.body;
+    const newLink = {
+        title,
+        url,
+        description
+    };
+    await pool.query('INSERT INTO links set ?', [newLink]);
+    //console.log(newLink);
     res.send('received');
 });
 
